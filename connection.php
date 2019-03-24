1b2181b079df3845a3dae47ea18384669ce9aa81
Abu Sayed Rubel <42999206+abusayedrubel@users.noreply.github.com>
2019-03-24 21:21:34 +0600

    no major change

--- connection.php
+++ connection.php
@@ -8,7 +8,7 @@
     $connect= mysqli_connect($db_host,$db_user,$db_pass,$db_name);
     
     if(mysqli_connect_error($connect)){
-        die('ami Sesh...'.mysqli_connect_error($connect));
+        die('ami Sesh...'.mysqli_connect_error($connect)); // if error found
     }else{
         return $connect;
         //echo 'ami parsi'; for text purpose
